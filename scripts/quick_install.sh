1c6f91413d07d930fdad9f142a27acb6f3ad3c61
Ole Herman Schumacher Elgesem <ole@northern.tech>
2021-04-22 18:13:57 -0500

    Removed quick install script
    Signed-off-by: Ole Herman Schumacher Elgesem <ole@northern.tech>

--- scripts/quick_install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-git clone https://github.com/olehermanse/cfbs.git
-cd ./cfbs || exit 1
-pip3 install -r requirements.txt
-chmod a+x ./cfbs/__main__.py
-ln -s $PWD/cfbs/__main__.py /usr/local/bin/cfbs
-ln -s $PWD/cfbs/__main__.py /usr/bin/cfbs
