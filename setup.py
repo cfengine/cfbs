4a06e2b082a6221c4296a9820db6484b01d38c02
Lars Erik Wik <lars.erik.wik@northern.tech>
2021-10-26 13:19:26 +0200

    Changed python version requirement from >=3.6 to >=3.5
    Ticket: CFE-3792
    Changelog: None
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.5',
     entry_points={
         "console_scripts": [
             "cfbs = cfbs.main:main"
