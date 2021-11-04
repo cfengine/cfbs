2230448d82445eea378cade530e53e0f2eace96c
Ole Herman Schumacher Elgesem <ole@northern.tech>
2021-11-04 20:31:19 +0100

    Marked install test as unsafe
    Signed-off-by: Ole Herman Schumacher Elgesem <ole@northern.tech>

--- test/shell/007_install.sh
+++ test/shell/007_install.sh
@@ -1,3 +1,11 @@
+if [ x"$UNSAFE_TESTS" = x1 ]
+then
+    echo "Unsafe tests are enabled - running install test"
+else
+    echo "Warning: Unsafe tests are disabled - skipping install test"
+    exit 0
+fi
+
 set -e
 set -x
 cd test
