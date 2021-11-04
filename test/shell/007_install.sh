5c7ab48439aad7188c663d020d4166e637ea8143
Ole Herman Schumacher Elgesem <ole@northern.tech>
2021-11-04 20:20:18 +0100

    install test: Check for augments file in home folder
    Signed-off-by: Ole Herman Schumacher Elgesem <ole@northern.tech>

--- test/shell/007_install.sh
+++ test/shell/007_install.sh
@@ -4,10 +4,12 @@ cd test
 mkdir -p ./tmp/
 cd ./tmp/
 touch cfbs.json && rm cfbs.json
+rm -rf ~/.cfagent/inputs/
 
 cfbs init
 cfbs add mpf
 cfbs add autorun
 cfbs install
 
-ls /var/cfengine/masterfiles/def.json
+ls ~/.cfagent/inputs/def.json
+rm -rf ~/.cfagent/inputs/
