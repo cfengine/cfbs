d8e2fea46b9f4ffd75274d37fe22f0438e7dc5c1
Ole Herman Schumacher Elgesem <ole@northern.tech>
2022-04-01 21:07:11 +0200

    Replaced test_showinfo.py with shell based test
    Signed-off-by: Ole Herman Schumacher Elgesem <ole@northern.tech>

---
+++ test/shell/017_info.sh
@@ -0,0 +1,19 @@
+set -e
+set -x
+cd test
+mkdir -p ./tmp/
+cd ./tmp/
+touch cfbs.json && rm cfbs.json
+rm -rf .git
+
+cfbs info masterfiles
+cfbs info masterfiles | grep "MPF"
+
+cfbs --non-interactive init
+
+cfbs info autorun
+cfbs info autorun | grep "Not added"
+
+cfbs --non-interactive add autorun
+cfbs info autorun
+cfbs info autorun | grep "Added"
