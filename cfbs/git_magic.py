85c1606097e58feef4b06fb96869be088201f8c7
Lars Erik Wik <lars.erik.wik@northern.tech>
2022-06-03 11:39:42 +0200

    Added a namedtuple for command results
    Added a namedtuple for command results, so that we can specify commit
    message bodies during command execution.
    
    Ticket: None
    Changelog: None

--- cfbs/git_magic.py
+++ cfbs/git_magic.py
@@ -4,10 +4,14 @@ Here it's okay to depend on other parts of the CFBS codebase,
 do prompts, etc.
 """
 
+from collections import namedtuple
 from cfbs.prompts import YES_NO_CHOICES, prompt_user
 from cfbs.cfbs_config import CFBSConfig, CFBSReturnWithoutCommit
 from cfbs.git import git_commit, git_discard_changes_in_file, CFBSGitError
 from functools import partial
+
+
+Result = namedtuple("Result", ["rc", "commit", "msg"])
 
 
 def git_commit_maybe_prompt(commit_msg, non_interactive, scope="all"):
@@ -40,15 +44,16 @@ def with_git_commit(
     def decorator(fn):
         def decorated_fn(*args, **kwargs):
             try:
-                ret = fn(*args, **kwargs)
+                result = fn(*args, **kwargs)
             except CFBSReturnWithoutCommit as e:
+                # Legacy; do not use. Use the 'Result' tuple instead.
                 return e.retval
+            ret, should_commit, msg = (
+                result if isinstance(result, Result) else (result, True, None)
+            )
 
-            config = CFBSConfig.get_instance()
-            if not config.get("git", False):
-                return ret
-
-            if ret in successful_returns:
+            # Message from the namedtuple overrides message from decorator
+            if not msg:
                 if positional_args_lambdas:
                     positional_args = (
                         l_fn(args, kwargs) for l_fn in positional_args_lambdas
@@ -57,20 +62,28 @@ def with_git_commit(
                 else:
                     msg = commit_msg
 
+            if not should_commit:
+                return ret
+
+            config = CFBSConfig.get_instance()
+            if not config.get("git", False):
+                return ret
+
+            if ret not in successful_returns:
+                return ret
+
+            try:
+                git_commit_maybe_prompt(msg, config.non_interactive, files_to_commit)
+            except CFBSGitError as e:
+                print(str(e))
                 try:
-                    git_commit_maybe_prompt(
-                        msg, config.non_interactive, files_to_commit
-                    )
+                    for file_name in files_to_commit:
+                        git_discard_changes_in_file(file_name)
                 except CFBSGitError as e:
                     print(str(e))
-                    try:
-                        for file_name in files_to_commit:
-                            git_discard_changes_in_file(file_name)
-                    except CFBSGitError as e:
-                        print(str(e))
-                    else:
-                        print("Failed to commit changes, discarding them...")
-                        return failed_return
+                else:
+                    print("Failed to commit changes, discarding them...")
+                    return failed_return
             return ret
 
         return decorated_fn
