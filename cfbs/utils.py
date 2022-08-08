7a8d921fd22843ddff2cda4804446065ee20630f
Lars Erik Wik <lars.erik.wik@northern.tech>
2022-08-08 19:14:08 +0200

    `read_json` now handles `NotADirectoryError` exception
    Ticket: None
    Changelog: None
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- cfbs/utils.py
+++ cfbs/utils.py
@@ -137,6 +137,8 @@ def read_json(path):
             return json.loads(f.read(), object_pairs_hook=OrderedDict)
     except FileNotFoundError:
         return None
+    except NotADirectoryError:
+        return None
     except json.decoder.JSONDecodeError as ex:
         print("Error reading json file {} : {}".format(path, ex))
         sys.exit(1)
