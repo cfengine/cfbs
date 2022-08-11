49042dc999814d616536c7610cca9844f125e3e2
Lars Erik Wik <lars.erik.wik@northern.tech>
2022-08-11 14:31:25 +0200

    `cfbs update` now supports modules with input
    Ticket: CFE-3953
    Changelog: Title
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- cfbs/commands.py
+++ cfbs/commands.py
@@ -14,6 +14,7 @@ from cfbs.utils import (
     cfbs_filename,
     is_cfbs_repo,
     item_index,
+    read_json,
     user_error,
     strip_right,
     pad_right,
@@ -26,7 +27,7 @@ from cfbs.utils import (
 )
 
 from cfbs.args import get_args
-from cfbs.pretty import pretty_check_file, pretty_file
+from cfbs.pretty import pretty, pretty_check_file, pretty_file
 from cfbs.build import init_out_folder, perform_build_steps
 from cfbs.cfbs_config import CFBSConfig, CFBSReturnWithoutCommit
 from cfbs.validate import CFBSIndexException, validate_index
@@ -47,6 +48,11 @@ from cfbs.git import (
 from cfbs.git_magic import Result, commit_after_command, git_commit_maybe_prompt
 from cfbs.prompts import YES_NO_CHOICES, prompt_user
 from cfbs.module import Module
+
+
+class InputDataUpdateFailed(Exception):
+    def __init__(self, message):
+        super().__init__(message)
 
 
 _MODULES_URL = "https://archive.build.cfengine.com/modules"
@@ -452,6 +458,108 @@ def _clean_unused_modules(config=None):
     return 0
 
 
+def update_input_data(module, input_data):
+    """
+    Update input data from module definition
+
+    :param module: Module with updated input definition
+    :param input_data: Input data to update
+    :return: True if changes are made
+    """
+    module_name = module["name"]
+    input_def = module["input"]
+
+    if len(input_def) != len(input_data):
+        raise InputDataUpdateFailed(
+            "Failed to update input data for module '%s': " % module_name
+            + "Input definition has %d variables, " % len(input_def)
+            + "while current input data has %d variables." % len(input_data)
+        )
+
+    def _update_keys(input_def, input_data, keys):
+        """
+        Update keys that can be safily updated in input data.
+        """
+        changes_made = False
+        for key in keys:
+            new = input_def.get(key)
+            old = input_data.get(key)
+            if new != old:
+                # Make sure that one of the keys are not 'None'
+                if new is None or old is None:
+                    raise InputDataUpdateFailed(
+                        "Failed to update input data for module '%s': " % module_name
+                        + "Missing matching attribute '%s'." % key
+                    )
+                input_data[key] = input_def[key]
+                changes_made = True
+                log.warning(
+                    "Updated attribute '%s' from '%s' to '%s' in module '%s'."
+                    % (key, old, new, module_name)
+                )
+        return changes_made
+
+    def _check_keys(input_def, input_data, keys):
+        """
+        Compare keys that cannot safily be updated for equality.
+        """
+        for key in keys:
+            new = input_def.get(key)
+            old = input_data.get(key)
+            if new != old:
+                raise InputDataUpdateFailed(
+                    "Failed to update input data for module '%s': " % module_name
+                    + "Updating attribute '%s' from '%s' to '%s'," % (key, old, new)
+                    + "may cause module to break."
+                )
+
+    def _update_variable(input_def, input_data):
+        _check_keys(input_def, input_data, ("type", "namespace", "bundle", "variable"))
+        changes_made = _update_keys(
+            input_def, input_data, ("label", "comment", "question", "while", "default")
+        )
+
+        if input_def["type"] == "list":
+            def_subtype = input_def["subtype"]
+            data_subtype = input_data["subtype"]
+            if type(def_subtype) != type(data_subtype):
+                raise InputDataUpdateFailed(
+                    "Failed to update input data for module '%s': " % module_name
+                    + "Different subtypes in list ('%s' != '%s')."
+                    % (type(def_subtype).__name__, type(data_subtype).__name__)
+                )
+            if isinstance(def_subtype, list):
+                if len(def_subtype) != len(data_subtype):
+                    raise InputDataUpdateFailed(
+                        "Failed to update input data for module '%s': " % module_name
+                        + "Different amount of elements in list ('%s' != '%s')."
+                        % (len(def_subtype), len(data_subtype))
+                    )
+                for i in range(len(def_subtype)):
+                    _check_keys(def_subtype[i], data_subtype[i], ("key", "type"))
+                    changes_made |= _update_keys(
+                        def_subtype[i],
+                        data_subtype[i],
+                        ("label", "question", "default"),
+                    )
+            elif isinstance(def_subtype, dict):
+                _check_keys(def_subtype, data_subtype, ("type",))
+                changes_made |= _update_keys(
+                    def_subtype, data_subtype, ("label", "question", "default")
+                )
+            else:
+                user_error(
+                    "Unsupported subtype '%s' in input definition for module '%s'."
+                    % (type(def_subtype).__name__, module_name)
+                )
+        return changes_made
+
+    changes_made = False
+    for i in range(len(input_def)):
+        changes_made |= _update_variable(input_def[i], input_data[i])
+    return changes_made
+
+
 @commit_after_command("Updated module%s", [PLURAL_S])
 def update_command(to_update):
     config = CFBSConfig.get_instance()
@@ -468,11 +576,22 @@ def update_command(to_update):
     new_deps_added_by = dict()
     changes_made = False
     msg = ""
+    files = []
     updated = []
 
     for update in to_update:
         module = config.get_module_from_build(update.name)
-        assert module is not None  # Checked above when logging skipped modules
+
+        custom_index = module is not None and "index" in module
+        index = Index(module["index"]) if custom_index else config.index
+
+        if not module:
+            index.translate_alias(update)
+            module = config.get_module_from_build(update.name)
+
+        if not module:
+            log.warning("Module '%s' not in build. Skipping its update." % update.name)
+            continue
 
         custom_index = module is not None and "index" in module
         index = Index(module["index"]) if custom_index else config.index
@@ -544,6 +663,38 @@ def update_command(to_update):
                             "Please make sure the old build steps work"
                             + " with the new version of the module"
                         )
+            elif key == "input":
+                if commit_differs:
+                    module["input"] = index_info["input"]
+
+                    input_path = os.path.join(".", module["name"], "input.json")
+                    input_data = read_json(input_path)
+                    if input_data == None:
+                        log.debug(
+                            "Skipping input update for module '%s': "
+                            + "No input found in '%s'" % (module["name"], input_path)
+                        )
+                    else:
+                        try:
+                            changes_made |= update_input_data(module, input_data)
+                        except InputDataUpdateFailed as e:
+                            log.warning(e)
+                            if prompt_user(
+                                config.non_interactive,
+                                "Input for module '%s' has changed " % module["name"]
+                                + "and may no longer be compatible. "
+                                + "Do you want to re-enter input now?",
+                                YES_NO_CHOICES,
+                                "no",
+                            ).lower() in ("no", "n"):
+                                continue
+                            input_data = copy.deepcopy(module["input"])
+                            config.input_command(module["name"], input_data)
+                            changes_made = True
+
+                    if changes_made:
+                        write_json(input_path, input_data)
+                        files.append(input_path)
             else:
                 if key == "dependencies":
                     extra = set(index_info["dependencies"]) - set(
@@ -578,16 +729,12 @@ def update_command(to_update):
     config.save()
 
     if changes_made:
-        if len(updated) > 1:
-            msg = "Updated %d modules\n" % len(updated) + msg
-        else:
-            assert updated
-            msg = msg[4:]  # Remove the '\n - ' part of the message
+        msg = "Updated %d module%s\n" % (len(updated), "s" if updated else "") + msg
         print("%s\n" % msg)
     else:
         print("Modules are already up to date")
 
-    return Result(0, changes_made, msg)
+    return Result(0, changes_made, msg, files)
 
 
 def validate_command():
