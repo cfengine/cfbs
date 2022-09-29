5ff39623ca9d79c386bdea12c5e7f39a1ff78bf0
Lars Erik Wik <lars.erik.wik@northern.tech>
2022-09-29 16:57:01 +0200

    Created `merge_json` unit test
    Created `merge_json` unit test to check my sanity while implementing the
    'input' buildstep.
    
    Ticket: None
    Changelog: None
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,35 @@
-from cfbs.utils import canonify
+from cfbs.utils import canonify, merge_json
 
 
 def test_canonify():
     assert canonify("Hello CFEngine!") == "Hello_CFEngine_"
     assert canonify("/etc/os-release") == "_etc_os_release"
     assert canonify("my-example-module") == "my_example_module"
+
+
+def test_merge_json():
+    original = {"classes": {"services_autorun": ["any"]}}
+    extras = {
+        "variables": {
+            "cfbs:create_single_file.filename": {
+                "value": "/tmp/create-single-file.txt",
+                "comment": "Added by 'cfbs input'",
+            }
+        }
+    }
+    merged = merge_json(original, extras)
+    assert "classes" in merged
+    assert "services_autorun" in merged["classes"]
+    assert merged["classes"]["services_autorun"] == ["any"]
+    assert "variables" in merged
+    assert "cfbs:create_single_file.filename" in merged["variables"]
+    assert "value" in merged["variables"]["cfbs:create_single_file.filename"]
+    assert (
+        merged["variables"]["cfbs:create_single_file.filename"]["value"]
+        == "/tmp/create-single-file.txt"
+    )
+    assert "comment" in merged["variables"]["cfbs:create_single_file.filename"]
+    assert (
+        merged["variables"]["cfbs:create_single_file.filename"]["comment"]
+        == "Added by 'cfbs input'"
+    )
