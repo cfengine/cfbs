4a606becc27410667100a6b0330b3d62c2c2d73c
Lars Erik Wik <lars.erik.wik@northern.tech>
2022-10-11 15:27:25 +0200

    Added message to assert in `prompt_user`
    Added message to assert in `prompt_user` in order to make it easier to
    debug when `default` is neither `None` nor in `choices`.
    
    Ticket: None
    Changelog: None
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- cfbs/prompts.py
+++ cfbs/prompts.py
@@ -12,7 +12,9 @@ def prompt_user(non_interactive, prompt, choices=None, default=None):
 
     prompt_separator = " " if prompt.endswith("?") else ": "
     if choices:
-        assert default is None or str(default) in choices
+        assert (
+            default is None or str(default) in choices
+        ), "'%s' not 'None' and '%s' not in '%s'" % (default, default, choices)
         choices_str = "/".join(
             choice.upper() if choice == str(default) else choice for choice in choices
         )
