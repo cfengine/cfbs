1222b51bb329bea7786ccb4b336ac1b916706151
Lars Erik Wik <lars.erik.wik@northern.tech>
2022-10-17 17:25:57 +0200

    Modify 010_local_add shell test
    Modify 010_local_add shell test to work with changes from CFE-3990.
    
    Ticket: CFE-3990
    Changelog: None
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- tests/shell/010_local_add.sh
+++ tests/shell/010_local_add.sh
@@ -8,18 +8,27 @@ rm -rf .git
 
 cfbs --non-interactive init
 cfbs status
-echo '
-bundle agent test_bundle
-{
-  meta:
-    "tags" slist => { "autorun" };
+
+echo 'bundle agent bogus {
   reports:
-    "test";
+      "This is $(this.promise_filename):$(this.bundle)!";
 }
-' > test_policy.cf
-cfbs --non-interactive add ./test_policy.cf
-grep '"name": "autorun"' cfbs.json
-grep '"name": "./test_policy.cf"' cfbs.json
+' > bogus.cf
+
+
+cfbs --non-interactive add ./bogus.cf
+
+grep '"name": "./bogus.cf"' cfbs.json
+grep '"policy_files ./bogus.cf"' cfbs.json
+grep '"bundles bogus"' cfbs.json
+
 cfbs status
 cfbs build
-ls out/masterfiles/services/autorun/test_policy.cf
+
+grep '"inputs"' out/masterfiles/def.json
+grep '"services/cfbs/bogus.cf"' out/masterfiles/def.json
+
+grep '"control_common_bundlesequence_end"' out/masterfiles/def.json
+grep '"bogus"' out/masterfiles/def.json
+
+ls out/masterfiles/services/cfbs/bogus.cf
