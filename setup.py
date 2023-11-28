4106f9870ec27ac1fcdb5e826de4bc527d466f6d
Ole Herman Schumacher Elgesem <ole.elgesem@northern.tech>
2023-11-29 00:52:01 +0100

    setup.py: Reformatted with black
    Signed-off-by: Ole Herman Schumacher Elgesem <ole.elgesem@northern.tech>

--- setup.py
+++ setup.py
@@ -2,13 +2,17 @@ import setuptools
 import subprocess
 import os
 
-cfbs_version = subprocess.run(['git', 'describe', '--tags'], stdout=subprocess.PIPE).stdout.decode("utf-8").strip()
+cfbs_version = (
+    subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE)
+    .stdout.decode("utf-8")
+    .strip()
+)
 if "-" in cfbs_version:
     # when not on tag, git describe outputs: "1.3.3-22-gdf81228"
     # pip has gotten strict with version numbers
     # so change it to: "1.3.3+22.git.gdf81228"
     # See: https://peps.python.org/pep-0440/#local-version-segments
-    v,i,s = cfbs_version.split("-")
+    v, i, s = cfbs_version.split("-")
     cfbs_version = v + "+" + i + ".git." + s
 
 assert "-" not in cfbs_version
@@ -31,19 +35,14 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/cfengine/cfbs",
     packages=setuptools.find_packages(exclude=["tests*"]),
-    package_data={'cfbs': ['VERSION']},
+    package_data={"cfbs": ["VERSION"]},
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.5',
-    entry_points={
-        "console_scripts": [
-            "cfbs = cfbs.main:main"
-        ]
-    },
-    install_requires=[
-    ],
+    python_requires=">=3.5",
+    entry_points={"console_scripts": ["cfbs = cfbs.main:main"]},
+    install_requires=[],
 )
