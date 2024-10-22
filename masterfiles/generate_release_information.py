676229916e6520bd1c8394badc97715bbe3109a8
jakub-nt <175944085+jakub-nt@users.noreply.github.com>
2024-10-22 19:31:01 +0200

    Remove extra dependencies; clean up code
    Signed-off-by: jakub-nt <175944085+jakub-nt@users.noreply.github.com>

--- masterfiles/generate_release_information.py
+++ masterfiles/generate_release_information.py
@@ -1,5 +1,5 @@
 # TODO document `cfbs generate-release-information`
-# this command uses several extra deps compared to the rest of cfbs
+# it generates the .json data files in the cwd
 import sys
 from pathlib import Path
 
@@ -7,14 +7,16 @@ from masterfiles.download_all_versions import download_all_versions_enterprise
 from masterfiles.check_tarball_checksums import check_tarball_checksums
 from masterfiles.generate_vcf_download import generate_vcf_download
 from masterfiles.generate_vcf_git_checkout import generate_vcf_git_checkout
-from masterfiles.check_download_matches_git import check_download_matches_git
+
+# commented out for now as this adds an extra dependency in its current state (dictdiffer)
+# from masterfiles.check_download_matches_git import check_download_matches_git
 
 ENTERPRISE_PATH = Path("./enterprise")
 
 
 def generate_release_information():
-    # only needs to be done once (although changes could happen afterwards), and silly to do if already have access to hosted files
     downloaded_versions, reported_checksums = download_all_versions_enterprise()
+    # TODO Community coverage:
     # downloaded_versions, reported_checksums = download_all_versions_community()
 
     # Enterprise 3.9.2 is downloaded but there is no reported checksum, so both args are necessary
@@ -29,5 +31,6 @@ def generate_release_information():
     generate_vcf_download(ENTERPRISE_PATH, downloaded_versions)
     generate_vcf_git_checkout(downloaded_versions)
 
-    check_download_matches_git(downloaded_versions)
-    # TODO automatic analysis of the difference-*.txts
+    # TODO automatic analysis of the difference between downloadable MPF data and git MPF data
+    # in its current state, this generates differences-*.txt files for each version
+    # check_download_matches_git(downloaded_versions)
