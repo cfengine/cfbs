dd63ef09b43487e6a1ea20a452a13b22f4c03589
Ole Herman Schumacher Elgesem <ole.elgesem@northern.tech>
2025-07-02 17:10:58 +0200

    'cfbs build' now includes cfbs.json into the policy set
    This file is useful, especially to see which modules were used and their
    versions.
    
    This is also less confusing than the current situation;
    
    'masterfiles' module is putting a cfbs.json there (its own). Other modules
    can do the same.
    
    Ticket: CFE-4553
    Signed-off-by: Ole Herman Schumacher Elgesem <ole.elgesem@northern.tech>

--- cfbs/build.py
+++ cfbs/build.py
@@ -1,5 +1,6 @@
 import os
 import logging as log
+import shutil
 from typing import List, Tuple
 from cfbs.utils import (
     canonify,
@@ -298,6 +299,8 @@ def perform_build(config) -> int:
     for module in config.get("build", []):
         for step in module["steps"]:
             _perform_build_step(module, step, module_name_length)
+    assert os.path.isdir("./out/masterfiles/")
+    shutil.copyfile("./cfbs.json", "./out/masterfiles/cfbs.json")
     if os.path.isfile("out/masterfiles/def.json"):
         pretty_file("out/masterfiles/def.json")
     print("")
