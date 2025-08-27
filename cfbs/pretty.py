ba2411aaa2243e10f124265e490b836b68954a56
Lars Erik Wik <lars.erik.wik@northern.tech>
2025-08-27 16:53:53 +0200

    pretty.py: un-nested functions to decrease complexity
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- cfbs/pretty.py
+++ cfbs/pretty.py
@@ -3,6 +3,9 @@ import re
 from copy import copy
 from collections import OrderedDict
 
+MAX_LEN = 80
+INDENT_SIZE = 2
+
 # Globals for the keys in cfbs.json and their order
 # Used for validation and prettifying / sorting.
 TOP_LEVEL_KEYS = ("name", "description", "type", "index", "git", "provides", "build")
@@ -216,95 +219,96 @@ def pretty_string(s, sorting_rules=None):
     return pretty(s, sorting_rules)
 
 
-def pretty(o, sorting_rules=None):
-    MAX_LEN = 80
-    INDENT_SIZE = 2
+def _should_wrap(parent, indent):
+    assert isinstance(parent, (tuple, list, dict))
+    # We should wrap the top level collection
+    if indent == 0:
+        return True
+    if isinstance(parent, dict):
+        parent = parent.values()
 
+    count = 0
+    for child in parent:
+        if isinstance(child, (tuple, list, dict)):
+            if len(child) >= 2:
+                count += 1
+    return count >= 2
+
+
+def _encode_list(lst, indent, cursor):
+    if not lst:
+        return "[]"
+    if not _should_wrap(lst, indent):
+        buf = json.dumps(lst)
+        assert "\n" not in buf
+        if indent + cursor + len(buf) <= MAX_LEN:
+            return buf
+
+    indent += INDENT_SIZE
+    buf = "[\n" + " " * indent
+    first = True
+    for value in lst:
+        if first:
+            first = False
+        else:
+            buf += ",\n" + " " * indent
+        buf += _encode(value, indent, 0)
+    indent -= INDENT_SIZE
+    buf += "\n" + " " * indent + "]"
+
+    return buf
+
+
+def _encode_dict(dct, indent, cursor):
+    if not dct:
+        return "{}"
+    if not _should_wrap(dct, indent):
+        buf = json.dumps(dct)
+        buf = "{ " + buf[1:-1] + " }"
+        assert "\n" not in buf
+        if indent + cursor + len(buf) <= MAX_LEN:
+            return buf
+
+    indent += INDENT_SIZE
+    buf = "{\n" + " " * indent
+    first = True
+    for key, value in dct.items():
+        if first:
+            first = False
+        else:
+            buf += ",\n" + " " * indent
+        if not isinstance(key, str):
+            raise ValueError("Illegal key type '" + type(key).__name__ + "'")
+        entry = '"' + key + '": '
+        buf += entry + _encode(value, indent, len(entry))
+    indent -= INDENT_SIZE
+    buf += "\n" + " " * indent + "}"
+
+    return buf
+
+
+def _encode(data, indent, cursor):
+    if data is None:
+        return "null"
+    elif data is True:
+        return "true"
+    elif data is False:
+        return "false"
+    elif isinstance(data, (int, float)):
+        return repr(data)
+    elif isinstance(data, str):
+        # Use the json module to escape the string with backslashes:
+        return json.dumps(data)
+    elif isinstance(data, (list, tuple)):
+        return _encode_list(data, indent, cursor)
+    elif isinstance(data, dict):
+        return _encode_dict(data, indent, cursor)
+    else:
+        raise ValueError("Illegal value type '" + type(data).__name__ + "'")
+
+
+def pretty(o, sorting_rules=None):
     if sorting_rules is not None:
         _children_sort(o, None, sorting_rules)
-
-    def _should_wrap(parent, indent):
-        assert isinstance(parent, (tuple, list, dict))
-        # We should wrap the top level collection
-        if indent == 0:
-            return True
-        if isinstance(parent, dict):
-            parent = parent.values()
-
-        count = 0
-        for child in parent:
-            if isinstance(child, (tuple, list, dict)):
-                if len(child) >= 2:
-                    count += 1
-        return count >= 2
-
-    def _encode_list(lst, indent, cursor):
-        if not lst:
-            return "[]"
-        if not _should_wrap(lst, indent):
-            buf = json.dumps(lst)
-            assert "\n" not in buf
-            if indent + cursor + len(buf) <= MAX_LEN:
-                return buf
-
-        indent += INDENT_SIZE
-        buf = "[\n" + " " * indent
-        first = True
-        for value in lst:
-            if first:
-                first = False
-            else:
-                buf += ",\n" + " " * indent
-            buf += _encode(value, indent, 0)
-        indent -= INDENT_SIZE
-        buf += "\n" + " " * indent + "]"
-
-        return buf
-
-    def _encode_dict(dct, indent, cursor):
-        if not dct:
-            return "{}"
-        if not _should_wrap(dct, indent):
-            buf = json.dumps(dct)
-            buf = "{ " + buf[1:-1] + " }"
-            assert "\n" not in buf
-            if indent + cursor + len(buf) <= MAX_LEN:
-                return buf
-
-        indent += INDENT_SIZE
-        buf = "{\n" + " " * indent
-        first = True
-        for key, value in dct.items():
-            if first:
-                first = False
-            else:
-                buf += ",\n" + " " * indent
-            if not isinstance(key, str):
-                raise ValueError("Illegal key type '" + type(key).__name__ + "'")
-            entry = '"' + key + '": '
-            buf += entry + _encode(value, indent, len(entry))
-        indent -= INDENT_SIZE
-        buf += "\n" + " " * indent + "}"
-
-        return buf
-
-    def _encode(data, indent, cursor):
-        if data is None:
-            return "null"
-        elif data is True:
-            return "true"
-        elif data is False:
-            return "false"
-        elif isinstance(data, (int, float)):
-            return repr(data)
-        elif isinstance(data, str):
-            # Use the json module to escape the string with backslashes:
-            return json.dumps(data)
-        elif isinstance(data, (list, tuple)):
-            return _encode_list(data, indent, cursor)
-        elif isinstance(data, dict):
-            return _encode_dict(data, indent, cursor)
-        else:
-            raise ValueError("Illegal value type '" + type(data).__name__ + "'")
 
     return _encode(o, 0, 0)
