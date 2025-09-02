ab3000648d90f607737d0b6868352cdf9226a76b
jakub-nt <175944085+jakub-nt@users.noreply.github.com>
2025-09-02 17:28:49 +0200

    Rewrote `CFBSCommandGitResult` for compatibility with Python 3.5
    Ticket: ENT-13212
    Signed-off-by: jakub-nt <175944085+jakub-nt@users.noreply.github.com>

--- cfbs/cfbs_types.py
+++ cfbs/cfbs_types.py
@@ -3,8 +3,34 @@ from typing import List, NamedTuple, Optional
 CFBSCommandExitCode = int
 
 
-class CFBSCommandGitResult(NamedTuple):
-    return_code: int
-    do_commit: bool = True
-    commit_message: Optional[str] = None
-    commit_files: List[str] = []
+# The usual, not `class`-based, method of defining a `typing.NamedTuple` type does not support default values,
+# and it seems that the `class`-based method's constructor does not work with the Python 3.5 PEP 484[1] comment type hints.
+# Thus, for compatibility with Python 3.5, a more verbose definition of `CFBSCommandGitResult` is used.
+# This commit can be reverted, and type hints returned to the PEP 526 style type hints[2], once the supported Python version becomes 3.6+.
+# References:
+# [1]: https://peps.python.org/pep-0484/#type-comments
+# [2]: https://peps.python.org/pep-0526/#class-and-instance-variable-annotations
+_CFBSCommandGitResult = NamedTuple(
+    "_CFBSCommandGitResult",
+    [
+        ("return_code", int),
+        ("do_commit", bool),
+        ("commit_message", Optional[str]),
+        ("commit_files", List[str]),
+    ],
+)
+
+
+class CFBSCommandGitResult(_CFBSCommandGitResult):
+    def __new__(
+        cls,
+        return_code: int,
+        do_commit: bool = True,
+        commit_message: Optional[str] = None,
+        commit_files: Optional[List[str]] = None,
+    ):
+        if commit_files is None:
+            commit_files = []
+        return super(CFBSCommandGitResult, cls).__new__(
+            cls, return_code, do_commit, commit_message, commit_files
+        )
