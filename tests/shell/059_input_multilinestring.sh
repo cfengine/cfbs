eeb078093797631b66e6e772a624d570ea83605b
Simon Halvorsen <simon.halvorsen@northern.tech>
2026-07-29 17:13:17 +0200

    ENT-14368: Added a new input-type: multilinestring
    Ticket: ENT-14368
    Signed-off-by: Simon Halvorsen <simon.halvorsen@northern.tech>

---
+++ tests/shell/059_input_multilinestring.sh
@@ -0,0 +1,17 @@
+set -e
+set -x
+cd tests/
+mkdir -p ./tmp/
+cd ./tmp/
+touch cfbs.json && rm cfbs.json
+rm -rf .git
+rm -rf create-single-file-with-content
+cp ../shell/059_input_multilinestring/example-cfbs.json cfbs.json
+
+cfbs --non-interactive input create-single-file-with-content
+grep '"type": "string-multiline"' create-single-file-with-content/input.json
+grep '"default": "Hello CFEngine!\\nBye CFEngine!"' create-single-file-with-content/input.json
+grep '"response": "Hello CFEngine!\\nBye CFEngine!"' create-single-file-with-content/input.json
+
+cfbs render-input create-single-file-with-content create-single-file-with-content/input.json actual.output
+diff actual.output ../shell/059_input_multilinestring/expected-augment.json
